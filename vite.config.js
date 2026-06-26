4504596b68923ab04c2b2a60ae976ff875dc1c2f
danathabtamu <142098362+danathabtamu@users.noreply.github.com>
2026-06-26 19:11:31 +0300

    Optimize 3D chunks to prevent build freezing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+   build: {
+    // 1. Prevents the build from choking on massive 3D chunk files
+    chunkSizeWarningLimit: 2000, 
+    rollupOptions: {
+      output: {
+        // 2. Separates Three.js into its own isolated background file
+        manualChunks: {
+          three: ['three', '@react-three/fiber', '@react-three/drei'],
+        },
+      },
+    },
+  },
 });
